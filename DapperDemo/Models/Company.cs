fca32cd2730a196e4570d3ff7865ce8d2f01dd82
Tanvir Islam <mdtanvirislams@gmail.com>
2024-05-02 02:01:02 +1000

    adding ef core project crud

---
+++ DapperDemo/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DapperDemo.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
